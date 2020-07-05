260d423793d9335afadc06cd5754cf27c5d6654d
Tim Castillo <tim.castillo@roche.com>
2020-07-05 09:44:47 -0700

    got a simple calculator working

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,20 +1,22 @@
 import React, { useContext, useEffect, useState } from 'react';
-// import { useForm } from "react-hook-form";
+import clsx from 'clsx';
 
 import Loader from '../Loader/Loader';
 
 import { FhirContext } from '../../context/FhirContext';
-import { StoreContext } from '../../context/storeContext';
 
 import usePatient from '../../parsers/usePatient';
+import useObservation from '../../parsers/useObservation';
 
 import './Main.scss';
-import useObservation from '../../parsers/useObservation';
+
+const DEFAULT_HEIGHT = 68;
+const DEFAULT_WEIGHT = 175
 
 const Main = () => {
   const fhir = useContext(FhirContext);
   const [height, setHeight] = useState();
-  const [weight, setWeight] = useState();
+  const [weight, setWeight] = useState(DEFAULT_WEIGHT);
 
   const {
     loading: patientLoading,
@@ -29,23 +31,68 @@ const Main = () => {
   } = useObservation(fhir);
 
   useEffect(() => {
-    setHeight(heightFhir?.value);
-    setWeight(weightFhir?.value);
+    setHeight(heightFhir ? heightFhir.value : DEFAULT_HEIGHT);
+    setWeight(weightFhir ? weightFhir.value : DEFAULT_WEIGHT);
   }, [heightFhir, weightFhir]);
 
-  console.log(height, weight);
+  const handleChangeHeight = (element) => {
+    setHeight(+element.target.value);
+  }
+
+  const handleChangeWeight = (element) => {
+    setWeight(+element.target.value);
+  }
+
+  const bmi = (703 * weight / (height ** 2)).toFixed(1);
+
+  let status = 'obese';
+
+  if (bmi < 18.5) {
+    status = 'underweight';
+  } else if (bmi < 25) {
+    status = 'healthy';
+  } else if (bmi < 30) {
+    status = 'overweight'
+  }
 
   return (
-    <div className='main'>
-      { patientLoading ? <Loader /> : <h1>{`${lastName}, ${firstName}`}</h1>}
+    <div className={clsx('main', status)}>
+      <div className="output-wrapper">
+        { observationLoading ? <Loader /> :
+          <span className='output'>{bmi}</span>
+        }
+      </div>
       <form className='form-wrapper'>
         <div className='input-wrapper'>
-          { !weight ? <Loader /> : <input type='number' value={weight}/> }
+          <span className='label'>{'Weight'}</span>
+          { observationLoading ? <Loader /> :
+            <>
+              <input
+                className='number-input'
+                type='number'
+                value={weight || 0}
+                onChange={handleChangeWeight}
+              />
+              <span className='label'>{`${weightFhir ? weightFhir.unit : 'lb'}${weight !== 1 ? 's' : ''}`}</span>
+            </>
+          }
         </div>
         <div className='input-wrapper'>
-          { !height ? <Loader /> : <input type='number' value={height}/> }
+          <span className='label'>{'Height'}</span>
+          { observationLoading ? <Loader /> :
+            <>
+              <input
+                className='number-input'
+                type='number'
+                value={height || 0}
+                onChange={handleChangeHeight}
+              />
+              <span className='label'>{`${heightFhir ? heightFhir.unit : 'in'}${height !== 1 ? 's' : ''}`}</span>
+            </>
+          }
         </div>
       </form>
+      
     </div>
   );
 };
--- src/parsers/useObservation.js
+++ src/parsers/useObservation.js
@@ -1,4 +1,5 @@
 import useResource from '../hooks/useResource';
+import { useState, useEffect } from 'react';
 
 const LOINC_SYSTEM = 'http://loinc.org';
 const LOINC_HEIGHT = ['91370-7'];
@@ -10,31 +11,32 @@ const useObservation = (client, params = {}) => {
   const queryStringArray = Object.keys(params).map(key => `${key}=${params[key]}`);
   const queryString = queryStringArray.length > 0 ? `?${queryStringArray.join('&')}` : '';
   const { resource, loading } = useResource(client, `Observation${queryString}`);
+  const [weight, setWeight] = useState();
+  const [height, setHeight] = useState();
 
-  if (loading) {
-    return { loading };
-  }
-
-  const observations = resource.entry ? resource.entry.map(({resource}) => resource) : [];
-  
-  console.log(observations);
-  let weight, height;
-
-  observations.forEach((observation) => {
-    if (observation.code.coding) {
-      observation.code.coding.forEach((coding) => {
-        if (coding.system === LOINC_SYSTEM) {
-          if (LOINC_HEIGHT.indexOf(coding.code) >= 0) {
-            height = observation.valueQuantity;
-          }
-
-          if (LOINC_WEIGHT.indexOf(coding.code) >= 0) {
-            weight = observation.valueQuantity;
-          }
-        }
-      });
+  useEffect(() => {
+    if (!resource) {
+      return
     }
-  });
+
+    const observations = resource.entry ? resource.entry.map(({resource}) => resource) : [];
+  
+    observations.forEach((observation) => {
+      if (observation.code.coding) {
+        observation.code.coding.forEach((coding) => {
+          if (coding.system === LOINC_SYSTEM) {
+            if (LOINC_HEIGHT.indexOf(coding.code) >= 0) {
+              setHeight(observation.valueQuantity)
+            }
+
+            if (LOINC_WEIGHT.indexOf(coding.code) >= 0) {
+              setWeight(observation.valueQuantity)
+            }
+          }
+        });
+      }
+    });
+  }, [resource]);
 
   return {
     resource,
